cf5002a44163155ce749269d590a1c5bd7a23041
Pushpendra Singh Rathore <pushpendrasinghrathore.it18@gmail.com>
2017-06-16 00:20:51 +0530

    Add files via upload

--- Client/startC.py
+++ Client/startC.py
@@ -2,9 +2,33 @@
 
 import  os,commands,sys,socket,time
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s_ip="192.168.10.191"
+#s_ip="192.168.10.191"
+#s_ip="192.168.43.48"
+s_ip="192.168.122.1"
 s_port=8888
 
+#Inform the server and providing with the ip of client to initiate the process of showing menu
+s.sendto("ok",(s_ip,s_port))
+
+#Storing received menu from server
+menu=s.recvfrom(1000)
+print menu[0]
+
+#option stores the suitable option choosen by the user as per the requirement
+option=raw_input("\nEnter your Choice : ")
+
+#Confirming the input  is the desired one only
+if option == '1' or option == '2':
+	#Sending choice to server
+	s.sendto(option,(s_ip,s_port))
+else:
+	print "Wrong choice ... Be Wise ... Thank You ..."
+	execfile('trig.py')
+
+#msg receives the message to be displayed to client to be interactive
+msg=s.recvfrom(100)
+print msg[0]
+"""
 drive_name=raw_input("enter  storage  drive  name  :  ")
 drive_size=raw_input("enter  drive  size MB :  ")
 s.sendto(drive_name,(s_ip,s_port))
@@ -18,6 +42,7 @@ if   res[0]  ==  "done" :
 
 else  :
 	print   "No response  from  storage  cloud "
+"""
 
 
 
---
+++ Client/trig.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+import os
+#file mainly used to display the content of startC.py again on wrong choice
+print "trig called"
+execfile('startC.py')
